0a89c06d86f4f86c15aaa64392a639ac2d02389a
kana-chan <bkm0115_y@icloud.com>
2020-02-24 17:46:48 +0900

    add info on create method (private)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,11 @@ class PostsController < ApplicationController
   end
 
   def create
-    
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.permit(:content)
   end
 end
